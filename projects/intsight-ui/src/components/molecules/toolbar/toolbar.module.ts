166fb0a0b9a4783b1de1964fe8a99f1088657315
ardiekevin <140492883+ardiekevin@users.noreply.github.com>
2023-08-31 11:11:45 +0800

    Update toolbar.module.ts

--- projects/intsight-ui/src/components/molecules/toolbar/toolbar.module.ts
+++ projects/intsight-ui/src/components/molecules/toolbar/toolbar.module.ts
@@ -3,18 +3,30 @@ import { CommonModule } from '@angular/common';
 import { ToolbarComponent } from './toolbar.component';
 import { IconButtonModule } from '../../atoms/icon-button/icon-button.module';
 import { SearchBoxModule } from '../search-box/search-box.module';
-import { AngularSvgIconModule } from 'angular-svg-icon';
 import { DropdownButtonModule } from '../dropdown-button/dropdown-button.module';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 @NgModule({
   declarations: [ToolbarComponent],
+  schemas: [
+    NO_ERRORS_SCHEMA,
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
   imports: [
     CommonModule,
     IconButtonModule,
     SearchBoxModule,
-    AngularSvgIconModule,
-    DropdownButtonModule
+    DropdownButtonModule, 
+    MatTooltipModule,
+    BrowserModule, 
+    FormsModule
   ],
-  exports: [ToolbarComponent]
+  exports: [
+    ToolbarComponent
+  ],
 })
 export class ToolbarModule { }
