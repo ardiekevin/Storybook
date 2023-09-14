f12c843c8bf7d001d25ca9f253b4a647939f0508
ardiekevin <140492883+ardiekevin@users.noreply.github.com>
2023-09-14 20:00:14 +0800

    Update datetime-picker.component.ts

--- projects/intsight-ui/src/components/molecules/datetime picker/datetime-picker.component.ts
+++ projects/intsight-ui/src/components/molecules/datetime picker/datetime-picker.component.ts
@@ -1,17 +1,40 @@
-import { Component } from '@angular/core';
-import { DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-datetime-picker',
   templateUrl: './datetime-picker.component.html',
-  styleUrls: ['./datetime-picker.component.scss'],
+  styleUrls: ['./datetime-picker.component.scss']
 })
-export class DatetimePickerComponent {
-  selectedDate: string = ''; // Initialize with an empty string
+export class DatetimePickerComponent implements OnInit {
+  selectedDate: string = '';
+  formattedDate: string = '';
+  dateForm!: FormGroup;
 
-  onSubmit() {
-    // Handle form submission logic here, e.g., send the selectedDate to the server
-    console.log('Selected Date:', this.selectedDate);
-    // You can implement your HTTP request or other logic here
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.dateForm = this.fb.group({
+      selectedDate: ['', [Validators.required, this.dateRangeValidator]]
+    });
+  }
+
+  dateRangeValidator(control: FormGroup) {
+    const selectedDate = new Date(control.value);
+
+    const minDate = new Date('2023-01-01');
+    const maxDate = new Date('2023-12-31');
+
+    return selectedDate >= minDate && selectedDate <= maxDate
+      ? null
+      : { dateRangeError: true };
+  }
+
+  onDateChange() {
+    const dateObj = new Date(this.selectedDate);
+    const day = dateObj.getDate().toString().padStart(2, '0');
+    const month = (dateObj.getMonth() + 1).toString().padStart(2, '0');
+    const year = dateObj.getFullYear();
+    this.formattedDate = `${day}.${month}.${year}`;
   }
 }
