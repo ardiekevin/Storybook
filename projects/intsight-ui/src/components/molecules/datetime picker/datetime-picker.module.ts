baa29bb6c6bdf9d8e700d442605d5f6c65ab3314
ardiekevin <140492883+ardiekevin@users.noreply.github.com>
2023-09-18 20:36:53 +0800

    Update datetime-picker.module.ts

--- projects/intsight-ui/src/components/molecules/datetime picker/datetime-picker.module.ts
+++ projects/intsight-ui/src/components/molecules/datetime picker/datetime-picker.module.ts
@@ -1,20 +1,17 @@
+import { DatetimePickerComponent } from './datetime-picker.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
 
-
-
-import { DatetimePickerComponent } from './datetime-picker.component';
 
 @NgModule({
-  declarations: [DatetimePickerComponent],
+  declarations: [
+    DatetimePickerComponent
+  ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
   ],
   bootstrap: [DatetimePickerComponent],
-  schemas: []
 })
 export class DatetimePickerModule { }
