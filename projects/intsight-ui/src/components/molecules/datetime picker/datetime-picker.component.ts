131f39bcd9fda09d9b85095d1adc1bc9b60ea3a4
ardiekevin <140492883+ardiekevin@users.noreply.github.com>
2023-09-20 10:23:42 +0800

    Update datetime-picker.component.ts

--- projects/intsight-ui/src/components/molecules/datetime picker/datetime-picker.component.ts
+++ projects/intsight-ui/src/components/molecules/datetime picker/datetime-picker.component.ts
@@ -1,29 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 
 @Component({
   selector: 'app-datetime-picker',
   templateUrl: './datetime-picker.component.html',
   styleUrls: ['./datetime-picker.component.scss']
 })
-export class DatetimePickerComponent {
-  selectedDate: Date | null = null;
+export class DatetimePickerComponent implements OnInit {
+  datePickerConfig: Partial<BsDatepickerConfig>;
 
-  formatDate(date: Date | null): string {
-    if (!date) return '';
-    const day = date.getDate().toString().padStart(2, '0');
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
-    const year = date.getFullYear();
-    return `${day}.${month}.${year}`;
+  constructor() {
+    this.datePickerConfig = {
+      containerClass: 'theme-blue',
+      dateInputFormat: 'DD.MM.YYYY', 
+    };
   }
 
-  onInputChange(event: Event) {
-    const inputValue = (event.target as HTMLInputElement).value;
-    const [day, month, year] = inputValue.split('.').map(Number);
-
-    if (!isNaN(day) && !isNaN(month) && !isNaN(year)) {
-      this.selectedDate = new Date(year, month - 1, day); 
-    } else {
-      this.selectedDate = null;
-    }
+  ngOnInit(): void {
   }
 }
