32bb62e8fd61a13359e599a28ac640f0a63d4c4f
ardiekevin <140492883+ardiekevin@users.noreply.github.com>
2023-09-25 20:43:48 +0800

    Update datetime-picker.component.ts

--- projects/intsight-ui/src/components/molecules/datetime picker/datetime-picker.component.ts
+++ projects/intsight-ui/src/components/molecules/datetime picker/datetime-picker.component.ts
@@ -1,30 +1,29 @@
-import { Component } from '@angular/core';
-import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-datetime-picker',
   templateUrl: './datetime-picker.component.html',
   styleUrls: ['./datetime-picker.component.scss']
 })
-export class DatetimePickerComponent {
-  isDropdownOpen = false;
-  selectedOption = 'dd.mm.yyyy';
-  selectedDate: Date | null = null;
-  bsConfig: Partial<BsDatepickerConfig> = {
-    containerClass: 'theme-default', // You can customize the date picker appearance here
-  };
+export class DatetimePickerComponent implements OnInit {
+  selectedDay!: number;
+  selectedMonth!: number;
+  selectedYear!: number;
 
-  toggleDropdown() {
-    this.isDropdownOpen = !this.isDropdownOpen;
+  days: number[] = Array.from({ length: 31 }, (_, i) => i + 1);
+  months: number[] = Array.from({ length: 12 }, (_, i) => i + 1);
+  years: number[] = Array.from({ length: 100 }, (_, i) => 2023 - i);
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.selectedDay = 1;
+    this.selectedMonth = 1;
+    this.selectedYear = 2023;
+    this.updateDate();
   }
 
-  selectOption(option: string) {
-    this.selectedOption = option;
-    this.isDropdownOpen = false;
-  }
-
-  onDateSelect(selectedDate: Date) {
-    console.log(`Selected date: ${selectedDate}`);
-    this.selectedDate = selectedDate;
+  updateDate(): void {
+    this.selectedDate = `${this.selectedDay.toString().padStart(2, '0')}.${this.selectedMonth.toString().padStart(2, '0')}.${this.selectedYear}`;
   }
 }
